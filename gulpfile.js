a6883cc8774503b71ce8e4be974881c243a1a311
Jeff Kielman <info@jeffkielman.com>
2017-11-21 22:33:46 -0500

    Add existing file

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+//gulpfile.js
+var gulp        = require('gulp');
+var deploy      = require('gulp-gh-pages');
+
+/**
+ * Push build to gh-pages
+ */
+gulp.task('deploy', function () {
+  return gulp.src("./dist/**/*")
+    .pipe(deploy())
+});
